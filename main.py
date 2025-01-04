5d4958d3e6cf89720877a8bf29e93346f5f9283f
Srijon Basak <srijonbasakom@gmail.com>
2025-01-04 23:41:39 +0600

    when cat jumps just down the cat

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ width, height = 600, 600
 xaxis = width/2
 yaxis = height/2
 
+
 cat_x = 0.0 #cat will be show from center
 cat_y = -50 #below change if needed
+
+food =[]
+food.append((xaxis-67, -(yaxis-35)))
+food.append((xaxis-57, -(yaxis-35)))
+food.append((xaxis-47, -(yaxis-35)))
+food.append((xaxis-37, -(yaxis-35)))
+food.append((xaxis-27, -(yaxis-35)))
+food.append((xaxis-17, -(yaxis-35)))
+
+hungry = 10
+
+
+def come_down(val):
+    global cat_y, hungry 
+    if cat_y > -50:
+        cat_y-=70
+    hungry += 0.5
+    glutPostRedisplay() 
 
