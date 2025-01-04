d3650c76cbbadc0d0f8af3e12c57b3befbe01c8a
Srijon Basak <srijonbasakom@gmail.com>
2025-01-04 21:22:56 +0600

    giving height width of the window and where the cat will be

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import random
+import math
+
+width, height = 600, 600
+
+#center
+xaxis = width/2
+yaxis = height/2
+
+cat_x = 0.0 #cat will be show from center
+cat_y = -50 #below change if needed
+
